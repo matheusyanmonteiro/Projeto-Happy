540817cf6565732b3e69c0dae6ed8d57b0f96621
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2020-10-14 14:29:27 -0300

    created controllers

--- backend/src/Routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import OrphanagesRouter from './orphanages.routes';
-
-const routes = Router();
-
-routes.use('/orphanages', OrphanagesRouter);
-
-export default routes;
--- backend/src/Routes/orphanages.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-function OrphanagesRouter() {
-
-}
-
-export default OrphanagesRouter;
